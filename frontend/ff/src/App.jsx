9fc5f7c65994bdb44af656ebfe26b5ea7994a656
anikas <22bec020@nith.ac.in>
2025-10-30 20:56:53 +0530

    routes fixed

--- frontend/ff/src/App.jsx
+++ frontend/ff/src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <h1 className="text-4xl font-bold text-blue-600 p-4">
-          Hello Tailwind!
-        </h1>
-      </div>
-    </>
+    <Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
